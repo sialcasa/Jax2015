28911119a5539843893af99fe7b75022f7eb49d3
Alexander Casall <alexander.casall@saxsys.de>
2015-03-12 22:28:34 +0100

    Styling

--- src/de/saxsys/javafx/jumpstart/Starter.java
+++ src/de/saxsys/javafx/jumpstart/Starter.java
@@ -1,11 +1,14 @@
 package de.saxsys.javafx.jumpstart;
 
+import insidefx.undecorator.Undecorator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Starter extends Application {
 
@@ -14,11 +17,12 @@ public class Starter extends Application {
 
         Parent load = FXMLLoader.load(getClass().getResource("JumpstartView.fxml"));
 
-        // Undecorator undecorator = new Undecorator(primaryStage, (Region) load);
-        // primaryStage.initStyle(StageStyle.TRANSPARENT);
-        // undecorator.getStylesheets().add("skin/undecorator.css");
-        // Scene scene = new Scene(undecorator, 1200, 700);
-        Scene scene = new Scene(load, 1200, 700);
+        Undecorator undecorator = new Undecorator(primaryStage, (Region) load);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        undecorator.getStylesheets().add("skin/undecorator.css");
+        undecorator.getStylesheets().add("de/saxsys/javafx/jumpstart/JumpstartView.css");
+        Scene scene = new Scene(undecorator, 1200, 800);
+
         scene.setFill(Color.TRANSPARENT);
         primaryStage.setScene(scene);
         primaryStage.show();
