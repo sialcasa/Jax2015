b71c8236dd148f99c3dc6b5c79c06562e979c475
Stefan Bley <stefan.bley@saxsys.de>
2015-03-13 12:14:32 +0100

    extract style object

--- src/main/java/de/saxsys/javafx/jumpstart/datacrawler/CrawlView.java
+++ src/main/java/de/saxsys/javafx/jumpstart/datacrawler/CrawlView.java
@@ -36,54 +36,77 @@ public class CrawlView {
 
         displayPerson.addListener((ChangeListener<Person>) (observable, oldValue, newValue) -> {
             WebEngine webEngine = wv_personinfo.getEngine();
-            webEngine.load(cb_source.getValue().getSearchUrl(newValue.getFirstname() + " " + newValue.getLastname()));
+            webEngine.load(cb_source.getValue().getSearchUrl(
+                    newValue.getFirstname() + " " + newValue.getLastname()));
         });
 
         cb_source.valueProperty().addListener(
                 (ChangeListener<CrawlTargets>) (observable, oldValue, newValue) -> {
                     WebEngine webEngine = wv_personinfo.getEngine();
                     webEngine.load(cb_source.getValue().getSearchUrl(
-                            displayPerson.get().getFirstname() + " " + displayPerson.get().getLastname()));
+                            displayPerson.get().getFirstname() + " "
+                                    + displayPerson.get().getLastname()));
                 });
 
-        wv_personinfo
-                .getEngine()
+        wv_personinfo.getEngine()
                 .documentProperty()
                 .addListener(
                         (ChangeListener<Document>) (observable, oldValue, newValue) -> {
-                            executejQuery(wv_personinfo.getEngine(), "");
-                            wv_personinfo.getEngine().executeScript(Scripts.getStyleObject);
-                            wv_personinfo.getEngine().executeScript(
-                                    "$(document).click(function(event) {"
-                                            + "$(event.target).closest('div').each(function() {"
-                                            + "var style = $(this).getStyleObject();" + "java.target(style);" + "});"
-                                            + "return false;" + "});");
+                            executeScript("$(document).click(function(event) {"
+                                    + "$(event.target).closest('div').each(function() {"
+                                    + "var style = $(this).getStyleObject();"
+                                    + "java.target(style);" + "});" + "return false;" + "});");
 
-                            JSObject jsobj = (JSObject) wv_personinfo.getEngine().executeScript("window");
+                            JSObject jsobj = (JSObject) executeScript("window");
                             jsobj.setMember("java", new Bridge());
 
+                            executejQuery("");
+
+                            executeScript(Scripts.getStyleObject);
                         });
 
-        pi_loading.visibleProperty().bind(
-                wv_personinfo.getEngine().getLoadWorker().stateProperty().isEqualTo(State.RUNNING));
-
+        bindLoadingIndicator();
     }
 
-    private static Object executejQuery(final WebEngine engine, String script) {
-        return engine.executeScript("(function(window, document, version, callback) { " + "var j, d;"
-                + "var loaded = false;" + "if (!(j = window.jQuery) || version > j.fn.jquery || callback(j, loaded)) {"
-                + " var script = document.createElement(\"script\");" + " script.type = \"text/javascript\";"
-                + " script.src = \"http://code.jquery.com/jquery-1.7.2.min.js\";"
-                + " script.onload = script.onreadystatechange = function() {"
-                + " if (!loaded && (!(d = this.readyState) || d == \"loaded\" || d == \"complete\")) {"
-                + " callback((j = window.jQuery).noConflict(1), loaded = true);" + " j(script).remove();" + " }"
-                + " };" + " document.documentElement.childNodes[0].appendChild(script) " + "} "
-                + "})(window, document, \"1.7.2\", function($, jquery_loaded) {" + script + "});");
+    private Object executeScript(String script) {
+        return wv_personinfo.getEngine().executeScript(script);
+    }
+
+    private Object executejQuery(String script) {
+        return wv_personinfo.getEngine()
+                .executeScript(
+                        "(function(window, document, version, callback) { "
+                                + "var j, d;"
+                                + "var loaded = false;"
+                                + "if (!(j = window.jQuery) || version > j.fn.jquery || callback(j, loaded)) {"
+                                + " var script = document.createElement(\"script\");"
+                                + " script.type = \"text/javascript\";"
+                                + " script.src = \"http://code.jquery.com/jquery-1.7.2.min.js\";"
+                                + " script.onload = script.onreadystatechange = function() {"
+                                + " if (!loaded && (!(d = this.readyState) || d == \"loaded\" || d == \"complete\")) {"
+                                + " callback((j = window.jQuery).noConflict(1), loaded = true);"
+                                + " j(script).remove();" + " }" + " };"
+                                + " document.documentElement.childNodes[0].appendChild(script) "
+                                + "} "
+                                + "})(window, document, \"1.7.2\", function($, jquery_loaded) {"
+                                + script + "});");
+    }
+
+    private void bindLoadingIndicator() {
+        pi_loading.visibleProperty().bind(
+                wv_personinfo.getEngine().getLoadWorker().stateProperty().isEqualTo(State.RUNNING));
     }
 
     public class Bridge {
-        public void target(JSObject content) {
-            System.out.println(content);
+
+        public void target(JSObject styleObject) {
+            JSObject keys = (JSObject) styleObject.eval("Object.keys(this);");
+            int arrayLength = (int) keys.getMember("length");
+            System.out.println(arrayLength + " style definitions:");
+            for (int i = 0; i < arrayLength; i++) {
+                String key = (String) keys.getSlot(i);
+                System.out.println(key + ": " + styleObject.getMember(key));
+            }
         }
     }
 
